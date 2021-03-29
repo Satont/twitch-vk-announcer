b8def8382562e1049b11229a978e633bf7f9464d
Satont <satontworldwide@gmail.com>
2021-03-29 07:36:52 +0300

    Make HOSTNAME variable be required

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ dotenv.config()
 import { expressBootstrap } from './express'
 import { posterBootstrap } from './poster'
 
-if (!process.env.TWITCH_USERID || !process.env.TWITCH_CLIENTID || !process.env.TWITCH_CLIENTSECRET || !process.env.VK_GROUP_TOKEN) {
+if (
+  !process.env.TWITCH_USERID ||
+  !process.env.TWITCH_CLIENTID ||
+  !process.env.TWITCH_CLIENTSECRET ||
+  !process.env.VK_GROUP_TOKEN ||
+  !process.env.HOSTNAME
+) {
   console.error('Not enough ENV variables for start application')
   process.exit(0)
 }
